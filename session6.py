bb7534cf22430898f3f0f1757315fe84f77b3773
Divyam Shah <divyam096@gmail.com>
2021-11-10 12:18:39 +0530

    Fix formatting in session6.py

--- session6.py
+++ session6.py
@@ -2,12 +2,13 @@ import math
 
 
 def check_docstring():
-    '''
+    """
     This function has been written to check if the docstring of an input function has greater than fifty \
     characters.
-    '''
+    """
     # Defining the minimum criteria length for docstring length
     doclength = 50
+
     def inner(func):
         # Using the docstring as a free variable
         nonlocal doclength
@@ -30,13 +31,14 @@ def check_docstring():
 
 
 def get_fibonacci_number():
-    '''
+    """
     This function has been written to check if an input number is a Fibonacci one and to generate the next Fibonacci number.
-    '''
+    """
     # Test to check for a perfect square
     func = lambda x: (int(math.sqrt(x)) ** 2) == x
     # Formula to calculate the next Fibonacci number
     nextfibonacci = lambda y: y *(1 + math.sqrt(5))/2
+
     def generate_fibonacci(numinp):
         if isinstance(numinp, int) == False:
             raise TypeError("Please enter an integer only")
@@ -52,9 +54,10 @@ def get_fibonacci_number():
 # Beginning of code corresponding to the third question of the assignment
 dictionary_of_functions = {}
 
+
 def track_arth_count(fn):
-    '''This function has been written in order to count how many times a few allowed functions - 'add', 'mul' and 'div' - have \
-    been called, and the respective counts are stored as values in a dictionary, with the corresponding function names acting as the keys.'''
+    """This function has been written in order to count how many times a few allowed functions - 'add', 'mul' and 'div' - have \
+    been called, and the respective counts are stored as values in a dictionary, with the corresponding function names acting as the keys."""
     if callable(fn) == False:
         raise TypeError("Please enter a valid function")
     if fn.__name__ not in ['add', 'mul', 'div']:
@@ -85,15 +88,14 @@ def div(a, b):
 
 
 def new_input_dictionary(fn, custom_dict):
-    '''This function has been written to take a dictionary - similar to the one created in the function track_arth_count \
-    - as input, and counting the number of times 'add', 'mul' and 'div' have been called.'''
+    """This function has been written to take a dictionary - similar to the one created in the function track_arth_count \
+    - as input, and counting the number of times 'add', 'mul' and 'div' have been called."""
     if callable(fn) == False:
         raise TypeError("Please enter a valid function")
     if (type(custom_dict) is dict) == False:
         raise TypeError("Please input only a dictionary")
     if fn.__name__ not in ['add', 'mul', 'div']:
         raise ValueError("Only add, mul, div are allowed")
-
 
     def inner(*args, **kwargs):
         if bool(args) == False:
